6d930beffaf99cc570f66b4261e390266ec33e8d
Michael <165153585+haystarkk@users.noreply.github.com>
2025-08-03 19:52:03 +0300

    Update query_samples.py

--- django-models/query_samples.py
+++ django-models/query_samples.py
@@ -1,35 +1,17 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-author_name = "Fred Connor"  
-authors = Author.objects.filter(name=author_name)
-if authors.exists():
-    author = authors.first()
-    books = Book.objects.filter(author=author)
-    for book in books:
-        print(book.title)
-else:
-    print("Author not found.")
+author = Author.objects.get(name="John Doe")
+books_by_author = Book.objects.filter(author=author)
+for book in books_by_author:
+    print(book.title)
 
 # List all books in a library
-library_name = "Central Library"  
-libraries = Library.objects.filter(name=library_name)
-if libraries.exists():
-    library = libraries.first()
-    books = library.books.all()
-    for book in books:
-        print(book.title)
-else:
-    print("Library not found.")
+library = Library.objects.get(name="Central Library")
+books_in_library = library.books.all()
+for book in books_in_library:
+    print(book.title)
 
 # Retrieve the librarian for a library
-if libraries.exists():
-    library = libraries.first()
-    librarians = Librarian.objects.filter(library=library)
-    if librarians.exists():
-        librarian = librarians.first()
-        print(librarian.name)
-    else:
-        print("Librarian not found.")
-else:
-    print("Library not found.")
+librarian = Librarian.objects.get(library=library)
+print(librarian.name)
