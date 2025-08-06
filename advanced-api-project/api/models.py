43d545a6cabbbb590200de6f402a2d3c180301ae
Michael <165153585+haystarkk@users.noreply.github.com>
2025-08-06 11:46:57 +0300

    Update models.py

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 
-# Author model stores basic information about book authors
 class Author(models.Model):
+    """
+    The Author model stores the name of book authors.
+    """
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
-# Book model stores book details and links to an Author
 class Book(models.Model):
+    """
+    The Book model stores book information including the title,
+    publication year, and the associated author (foreign key).
+    """
     title = models.CharField(max_length=200)
     publication_year = models.IntegerField()
     author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
