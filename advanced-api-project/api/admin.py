1cd8dfd54fda82570a60f23a00e92d5ef3960d1a
Michael <165153585+haystarkk@users.noreply.github.com>
2025-08-07 13:37:45 +0300

    Update admin.py

--- advanced-api-project/api/admin.py
+++ advanced-api-project/api/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Author, Book
 
-admin.site.register(Author)
-admin.site.register(Book)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'book_count')
+    search_fields = ('name',)
+
+    def book_count(self, obj):
+        return obj.books.count()
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('author', 'publication_year')
+    search_fields = ('title', 'author__name')
