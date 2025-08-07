6b5e26d968c3bfc5fbbf4e3c21f227a6f1a9518a
Michael <165153585+haystarkk@users.noreply.github.com>
2025-08-07 14:04:38 +0300

    Update models.py

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Author(models.Model):
     """Represents an author who writes books"""
@@ -9,6 +12,7 @@ class Author(models.Model):
 
 class Book(models.Model):
     """Represents a book written by an author"""
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='books')
     title = models.CharField(max_length=200)
     publication_year = models.IntegerField()
     author = models.ForeignKey(
