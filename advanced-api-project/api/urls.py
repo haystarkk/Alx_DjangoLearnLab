3e11a723f1d0016b9d75b41f597ed1ddf1e74bb8
Michael <165153585+haystarkk@users.noreply.github.com>
2025-08-07 14:11:45 +0300

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from . import views
+from rest_framework.permissions import IsAuthenticated
 
 urlpatterns = [
     # Book endpoints
     path('books/', views.BookListView.as_view(), name='book-list'),
     path('books/create/', views.BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
-    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book-delete'),
+    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), 
+        name='book-update'),
+    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), 
+        name='book-delete'),
     
     # Author endpoints
     path('authors/', views.AuthorListView.as_view(), name='author-list'),
