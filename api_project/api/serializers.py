06b76ca924269a3abe99531cbb09f2a11c26378c
Michael <165153585+haystarkk@users.noreply.github.com>
2025-08-07 16:01:48 +0300

    Update serializers.py

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -4,4 +4,4 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = '__all__'  # Includes all fields from the Book model
