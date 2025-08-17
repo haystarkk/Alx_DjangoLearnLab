a71f64e5938339e86cecee8ba89aa09bbc7cdee9
Michael <165153585+haystarkk@users.noreply.github.com>
2025-08-17 20:21:34 +0300

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,16 @@
-# blog/forms.py
 from django import forms
-from .models import Comment
+from .models import Post, Comment
+from taggit.forms import TagWidget
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'tags': TagWidget(attrs={'class': 'form-control'}),
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
