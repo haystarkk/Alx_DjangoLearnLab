969904aaea3965e6ecce77474f9236c3f9efc868
Michael <165153585+haystarkk@users.noreply.github.com>
2025-08-17 21:35:05 +0300

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,20 +1,32 @@
 # blog/forms.py
 from django import forms
 from .models import Post, Comment
-from taggit.forms import TagWidget
+from taggit.forms import TagWidget  # Explicit import of TagWidget
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
 class PostForm(forms.ModelForm):
+    tags = forms.CharField(
+        widget=TagWidget(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter tags separated by commas'
+        }),
+        required=False,
+        help_text="Add relevant tags separated by commas"
+    )
+
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 6}),
-            'tags': TagWidget(attrs={
+            'title': forms.TextInput(attrs={
                 'class': 'form-control',
-                'placeholder': 'Add tags separated by commas'
+                'placeholder': 'Enter post title'
+            }),
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 6,
+                'placeholder': 'Write your post content here...'
             }),
         }
 
@@ -22,6 +34,10 @@ class PostForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.add_input(Submit('submit', 'Submit', css_class='btn-primary'))
+
+    def clean_tags(self):
+        tags = self.cleaned_data.get('tags', '')
+        return [tag.strip() for tag in tags.split(',') if tag.strip()]
 
 class CommentForm(forms.ModelForm):
     class Meta:
